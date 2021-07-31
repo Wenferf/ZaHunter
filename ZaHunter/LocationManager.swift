e865e6fe1f4942bea746aee3ea728327308d931e
Tucker Gillbrand <tucker@gillbrand.com>
2021-07-30 23:10:15 -0700

    Added map search feature

---
+++ ZaHunter/LocationManager.swift
@@ -0,0 +1,19 @@
+//
+//  LocationManager.swift
+//  ZaHunter
+//
+//  Created by Tucker(School) on 7/30/21.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+            super.init()
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+}
